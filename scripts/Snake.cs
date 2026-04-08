d7b96c39058b710fa9e4452430518e4237b4cdb6
Minseob Kim <33077839+msisdev@users.noreply.github.com>
2026-04-08 18:17:27 +0900

    snake moves smoothly

--- scripts/Snake.cs
+++ scripts/Snake.cs
@@ -1,104 +1,103 @@
 using Godot;
+using System;
 using System.Collections.Generic;
 
 public partial class Snake : Node2D
 {
-  // Snake movement speed
-	[Export] public float Speed = 200.0f;
-  // Distance between each body segment
-	[Export] public float BodyDist = 10.0f;
-  // Minimum distance the head must be from the mouse to move towards it
-  [Export] public float HeadMouseMoveDistance = 5.0f;
-  // Snake segment scene
-	private PackedScene _segmentScene = GD.Load<PackedScene>("res://scenes/snake_segment.tscn");
-  // List of segment nodes
-	private List<Node2D> _body = new List<Node2D>();
-  // Reference to the head segment
-  private Node2D _head => _body.Count > 0 ? _body[0] : null;
-  // Path data manager
-	private SnakePath _path;
+	[Export] private float _spd = 200f;
+	[Export] private float _rspd = 5f;
+	[Export] private Vector2 _face = Vector2.Up;
+	[Export] private PackedScene _segmentScene = ResourceLoader.Load<PackedScene>("res://scenes/snake_segment.tscn");
+	[Export] private int _initBodyCount = 10;
+	[Export] private float _segmentSpacing = 5f;
 
+	private Node2D _head;
+	private List<Node2D> _body = new List<Node2D>();
+	private List<Vector2> _path = new List<Vector2>();
+	
 	public override void _Ready()
 	{
-		AddSegment(GlobalPosition);
-		_path = new SnakePath(BodyDist, _body[0].GlobalPosition);
+		// Randomize the initial face direction.
+		_face = Vector2.FromAngle((float)(Random.Shared.NextDouble() * Math.PI * 2));
+
+		// Put head.
+		{
+			_head = _segmentScene.Instantiate<Node2D>();
+			_head.Position = Position;
+			AddChild(_head);
+			_path.Insert(0, _head.Position);
+		}
+
+		// Add body segments.
+		for (int i = 0; i < _initBodyCount; i++)
+		{
+			_Grow();
+		}
 	}
 
 	public override void _Process(double delta)
 	{
-		float fDelta = (float)delta;
-
 		if (Input.IsMouseButtonPressed(MouseButton.Left))
 		{
-			MoveSnake(fDelta);
+			_Move(delta);
 		}
-
-		if (Input.IsKeyPressed(Key.Space))
+		if (Input.IsPhysicalKeyPressed(Key.Space))
 		{
-			AddSegment(_body[^1].GlobalPosition);
+			_Grow();
 		}
 	}
 
-  /// <summary>
-  /// To move the snake,
-  /// <list type="bullet">Compare distance between head and mouse.</list>
-  /// <list type="bullet">If the distance is greater than the threshold, update the head and body position.</list>
-  /// </summary>
-	private void MoveSnake(float delta)
-	{
-    Vector2   headAt    = _head.GlobalPosition;
-		Vector2   mouseAt   = GetGlobalMousePosition();
-		Vector2   dir       = (mouseAt - _head.GlobalPosition).Normalized();
-
-		// Compare head-mouse dist with threshold.
-	  if (headAt.DistanceTo(mouseAt) > HeadMouseMoveDistance)
-		{
-			// With the new head position, add the new points to the path.
-			_path.UpdatePath(headAt + dir * Speed * delta, _body.Count);
-
-			// Move body positions.
-			MoveSnakeBody();
-
-			// Move head position.
-			_head.GlobalPosition = headAt + dir * Speed * delta;
-		}
-	}
-
-	private void MoveSnakeBody()
-	{
-		if (_path.Count == 0) return;
-		
-		// Get dist between: current head position to the new head position
-		float headTravelDist = _body[0].GlobalPosition.DistanceTo(_path[0]);
-
-		// 
-		float lerpFactor = Mathf.Clamp(headTravelDist / BodyDist, 0f, 1f);
-
-		for (int i = 1; i < _body.Count; i++)
-		{
-			// Move body[i] if there is a point[i] for it
-			if (i < _path.Count)
-			{
-				Vector2 startPos = _path[i];
-				Vector2 endPos = _path[i - 1];
-
-				// 
-				_body[i].GlobalPosition = startPos.Lerp(endPos, lerpFactor);
-			}
-			// Otherwise, the body[i] should be overlapped with the last point.
-			else
-			{
-				_body[i].GlobalPosition = _path.Last;
-			}
-		}
-	}
- 
-	private void AddSegment(Vector2 spawnPos)
+	// Add a new body segment.
+	private void _Grow()
 	{
 		var newSegment = _segmentScene.Instantiate<Node2D>();
-		newSegment.GlobalPosition = spawnPos;
-
-		AddChild(newSegment);
+		newSegment.Position = _body.Count > 0 ? _body[^1].Position : _head.Position;
 		_body.Add(newSegment);
+		AddChild(newSegment);
+	}
+
+	// Move the snake
+	private void _Move(double delta)
+	{
+		// Rotate the face vector towards the mouse.
+		{
+			var mousePos = GetGlobalMousePosition();
+			var toMouse = (mousePos - _head.GlobalPosition).Normalized();
+			_face = _face.Lerp(toMouse, (float)delta * _rspd).Normalized();
+		}
+
+		// Move head.
+		{
+			_head.Position += _face * _spd * (float)delta;
+		}
+
+		// Update path.
+		{
+			if (_head.Position.DistanceTo(_path[0]) >= _segmentSpacing)
+			{
+				_path.Insert(0, _head.Position);
+				while (_path.Count > _body.Count + 1)
+				{
+					_path.RemoveAt(_path.Count - 1);
+				}
+			}
+		}
+
+		// Move bodies.
+		{
+			var headToPathLen = _head.Position.DistanceTo(_path[0]);
+			var lerpWeight = Math.Min(1f, headToPathLen / _segmentSpacing);
+			for (int i = 0; i < _body.Count; i++)
+			{
+				if (i + 1 < _path.Count)
+				{
+					_body[i].Position = _path[i + 1].Lerp(_path[i], lerpWeight);
+				}
+				else if (_path.Count > 0)
+				{
+					_body[i].Position = _path[^1];
+				}
+			}
+		}
 	}
 }
